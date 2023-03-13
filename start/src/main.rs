33d10eff522457c7ffa600cf9dc471afa093f7a1
alsfkl222 <alskfl222@kakao.com>
2023-03-13 17:09:09 +0900

    [ FEAT ] : test regex and simple struct

--- start/src/main.rs
+++ start/src/main.rs
@@ -4,10 +4,18 @@ use std::thread;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
 use std::time::Duration;
-use chrono::prelude::Utc;
+use chrono::{DateTime, Utc};
 use reqwest::{Error, blocking::get};
 use scraper::Html;
-use module::{get_text, get_urls};
+use module::{get_text, parse_temp};
+
+#[derive(Debug)]
+struct Weather {
+    time: DateTime<Utc>,
+    location: String,
+    temperature: f32,
+    status: String
+}
 
 fn main() -> Result<(), Error> {
     let running = Arc::new(AtomicBool::new(true));
@@ -17,15 +25,20 @@ fn main() -> Result<(), Error> {
     }).expect("Error setting Ctrl-C handler");
 
     while running.load(Ordering::SeqCst) {
-        let now = Utc::now();
-        println!("{}", now);
         let response = get("https://weather.naver.com/today/05110101")?.text()?;
         let document = Html::parse_document(&response);
-        let title = get_text(&document, "div#now h2").unwrap();
+        let now = Utc::now();
         let location = get_text(&document, "div#now strong.location_name").unwrap();
-        let links = get_urls(&document, "a").unwrap();
-        println!("{}, {}", title, location);
-        println!("{:?}", links);
+        let temp_text = get_text(&document, "div#now strong.current").unwrap();
+        let temperature = parse_temp(&temp_text).unwrap();
+        let status = get_text(&document, "div#now span.weather").unwrap();
+        let info = Weather {
+            time: now,
+            location,
+            temperature,
+            status
+        };
+        println!("{:?}", info);
         println!();
         thread::sleep(Duration::from_secs(1));
     }
--- start/src/module.rs
+++ start/src/module.rs
@@ -1,4 +1,5 @@
 use std::result::Result;
+use regex::Regex;
 use reqwest::Error;
 use scraper::{Html, Selector};
 
@@ -10,15 +11,22 @@ pub fn get_text(document: &Html, selector: &str) -> Result<String, Error> {
   Ok(title)
 }
 
-pub fn get_urls(document: &Html, selector: &str) -> Result<Vec<String>, Error> {
-  let selector_parse = Selector::parse(&selector).unwrap();
-  let elements = document.select(&selector_parse).collect::<Vec<_>>();
-  let mut contents = Vec::new();
-  for element in &elements {
-      let string = element.value().attr("href").unwrap().to_string();
-      if string.chars().next() != Some('#') {
-        contents.push(string);
-      }
-  };
-  Ok(contents)
+// pub fn get_urls(document: &Html, selector: &str) -> Result<Vec<String>, Error> {
+//   let selector_parse = Selector::parse(&selector).unwrap();
+//   let elements = document.select(&selector_parse).collect::<Vec<_>>();
+//   let mut contents = Vec::new();
+//   for element in &elements {
+//       let string = element.value().attr("href").unwrap().to_string();
+//       if string.chars().next() != Some('#') {
+//         contents.push(string);
+//       }
+//   };
+//   Ok(contents)
+// }
+
+pub fn parse_temp(string: &String) -> Result<f32, Error> {
+  let re = Regex::new(r"\d+\.\d+|\d+").unwrap();
+  let captures = re.find_iter(string);
+  let numbers: Vec<f32> = captures.map(|capture| capture.as_str().parse().unwrap()).collect();
+  Ok(numbers[0])
 }
